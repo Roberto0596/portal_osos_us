eb478d4d5aa345828c1541d55d81d0fcbef87bbd
MocTey <davidyanez.y@hotmail.com>
2020-06-19 20:00:29 -0700

    vista de login

--- resources/views/Alumn/Auth/login.blade.php
+++ resources/views/Alumn/Auth/login.blade.php
@@ -2,34 +2,89 @@
 
 @section('content')
 
-<div class="login-form">
+<link rel="stylesheet" href="{{asset("css/app.css")}}">
 
-    <form action="" method="post">
 
-        <h2 class="text-center">Bienvenido</h2>  
-
-        <div class="form-group">
-
-            <input type="text" class="form-control" placeholder="Username" required="required">
-
-        </div>
-
-        <div class="form-group">
-
-            <input type="password" class="form-control" placeholder="Password" required="required">
-
-        </div>
-
-        <div class="form-group">
-
-            <button type="submit" class="btn btn-primary btn-block">Log in</button>
-
-        </div>
-
-    </form>
 
     
 
+
+
+
+
+
+
+<body class="imagen-fondo">
+<nav class="navbar navbar-expand-lg navbar-light navbar-orange">
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+
+	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	    <ul class="navbar-nav mr-auto">
+	      <li class="nav-item active">
+	        <a class="nav-link" href="{{route('home')}}">Inicio</a>
+	      </li>
+	      <li class="nav-item active">
+	        <a class="nav-link" href="{{route('home')}}">Plan de estudios</a>
+	      </li>
+	    </ul>
+	  </div>
+	</nav>
+
+<div class="bienvenida">
+    <h2>
+        Bienvenido oso, tenemos estas noticias para ti!
+    </h2>
 </div>
+    <div class="login-form">
+
+        <form action="" method="post">
+
+            <h2 class="text-center">Iniciar sesión</h2>  
+
+            <div class="form-group">
+
+            <input type="text" class="form-control" placeholder="Correo" required="required">
+
+             </div>
+
+            <div class="form-group">
+
+                <input type="password" class="form-control" placeholder="Contraseña" required="required">
+
+            </div>
+
+            <div class="form-group">
+
+                <button type="submit" class="btn btn-primary btn-block boton">Entrar</button>
+
+            </div>
+        </form> 
+    </div>
+
+    <div class="feed">
+
+		<div class="feed_content">
+
+			<div class="feed-header">
+				<h1>encabezado2</h1>
+			</div>
+
+			<div class="feed-body">
+				<p>Parrafo</p>
+			</div>
+
+		</div>
+						
+	</div>
+</body>
+
+
+
+
+
+
+
 
 @stop
