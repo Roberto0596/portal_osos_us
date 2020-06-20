5c46b58a17cc0e465701c5c3ca05f7be903033f5
MocTey <davidyanez.y@hotmail.com>
2020-06-20 16:04:36 -0700

    login corregido

--- resources/views/Alumn/Auth/login.blade.php
+++ resources/views/Alumn/Auth/login.blade.php
@@ -3,27 +3,46 @@
 @section('content')
 
 <div class="imagen-fondo">
-
+   
     @include('Website.header')
 
     <div class="bienvenida">
         <h2>
-            Bienvenido oso, tenemos estas noticias para ti!
+        Bienvenido oso, tenemos estas noticias para ti!
         </h2>
+
+    <div class="feed2">
+
+        <div class="feed_content">
+
+            <div class="feed-header">
+                <h1>encabezado2</h1>
+            </div>
+
+            <div class="feed-body">
+                <p>Parrafo</p>
+            </div>
+
+        </div>
+                    
     </div>
+    
+    </div>
+
+
     <div class="login-form">
 
         <form action="" method="post">
 
-             {{ csrf_field() }}
+            {{ csrf_field() }}
 
             <h2 class="text-center">Iniciar sesión</h2>  
 
             <div class="form-group">
 
-            <input type="text" class="form-control" placeholder="Correo" required="required" name="email">
+                <input type="text" class="form-control" placeholder="Correo" required="required" name="email">
 
-             </div>
+            </div>
 
             <div class="form-group">
 
@@ -39,29 +58,49 @@
         </form> 
     </div>
 
-    <div class="feed2">
-
-		<div class="feed_content">
-
-			<div class="feed-header">
-				<h1>encabezado2</h1>
-			</div>
-
-			<div class="feed-body">
-				<p>Parrafo</p>
-			</div>
-
-		</div>
-						
-	</div>
-
+    <div>hola</div>
 </div>
 
 
 
 
 
-
-
-
 @stop
+
+
+
+
+<!--<div class="bienvenida">
+    <h2>
+        Bienvenido oso, tenemos estas noticias para ti!
+    </h2>
+</div>
+<div class="login-form">
+
+    <form action="" method="post">
+
+         {{ csrf_field() }}
+
+        <h2 class="text-center">Iniciar sesión</h2>  
+
+        <div class="form-group">
+
+        <input type="text" class="form-control" placeholder="Correo" required="required" name="email">
+
+         </div>
+
+        <div class="form-group">
+
+            <input type="password" class="form-control" placeholder="Contraseña" required="required" name="password">
+
+        </div>
+
+        <div class="form-group">
+
+            <button type="submit" class="btn btn-primary btn-block boton">Entrar</button>
+
+        </div>
+    </form> 
+</div>
+
+-->
