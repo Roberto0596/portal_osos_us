229a1cc65389761faa6bd632b03f836daa43456e
MocTey <davidyanez.y@hotmail.com>
2020-07-08 18:14:51 -0700

    fichas de desposito generadas listas

--- resources/views/pdf/index.blade.php
+++ resources/views/pdf/index.blade.php
@@ -4,18 +4,25 @@
 
 <form  method="POST" action="{{ route('alumn.cedula',['digital','ver'])}}" style="width: 40%; margin: 5%;">
     @csrf
-    <div class="form-group">
-    	<img src="{{ asset('img/temple/unisierra.png') }}" alt="">
-        <label for="matricula">Matrícula del alumno</label>
-        <input type="text" is_null="false" class="form-control" name="matriculaAlumno" id="matricula" aria-describedby="matriculaHelp" placeholder="Ingrese matrícula">
-    </div>
-    <button type="submit" class="btn btn-primary" style="background-color: orange; border: none; float: left;">Cedula</button>
+   
+   
+    <button type="submit" class="btn btn-primary" style="background-color: orange; border: none; float: left;margin: 10%">Cedula</button>
 </form>
 <form  method="POST" action="{{ route('alumn.constancia',['digital','ver'])}}" style="width: 40%; margin: 5%;">
     @csrf
-    <div class="form-group">
-        <label for="matricula">Matrícula del alumno</label>
-        <input type="text" is_null="false" class="form-control" name="matriculaAlumno" id="matricula" aria-describedby="matriculaHelp" placeholder="Ingrese matrícula">
-    </div>
-    <button type="submit" class="btn btn-primary" style="background-color: orange; border: none; float: left;">Cedula</button>
+    
+    
+    <button type="submit" class="btn btn-primary" style="background-color: orange; border: none; float: left;margin: 10%">Constancia</button>
+</form>
+<form  method="POST" action="{{ route('alumn.fichas',['digital','ver','transferencia'])}}" style="width: 40%; margin: 5%;">
+    @csrf
+    
+    
+    <button type="submit" class="btn btn-primary" style="background-color: orange; border: none; float: left; margin: 10%">Transferencia</button>
+</form>
+<form  method="POST" action="{{ route('alumn.fichas',['digital','ver','deposito'])}}" style="width: 40%; margin: 5%;">
+    @csrf
+    
+    
+    <button type="submit" class="btn btn-primary" style="background-color: orange; border: none; float: left; margin: 10%">Depósito</button>
 </form>
