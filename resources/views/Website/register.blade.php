3e89d9023df11c5c293aa64cccf39de3b0ab40b2
luis8elias <luiseliashumar@gmail.com>
2020-07-19 22:39:26 -0700

    algunos correciones de texto

--- resources/views/Website/register.blade.php
+++ resources/views/Website/register.blade.php
@@ -88,6 +88,12 @@
 
 						<div class="row">
 
+							<h3>Registrarme en el Portal</h3>
+
+						</div>
+
+						<div class="row">
+
 							<div class="col-md-12">
 								
 								<div class="input-group mb-3">
