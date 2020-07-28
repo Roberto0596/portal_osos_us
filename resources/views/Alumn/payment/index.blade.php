a438b0752a982cbabd8c937114b633a630d76675
luis8elias <luiseliashumar@gmail.com>
2020-07-28 16:05:26 -0700

    inc de anio de vencimineto tarjeta

--- resources/views/Alumn/payment/index.blade.php
+++ resources/views/Alumn/payment/index.blade.php
@@ -378,9 +378,6 @@
             <div class="select">
               <select data-conekta="card[exp_year]"  class="form-control" id="expire-year">
                 <option></option>
-                <option value="2016">2016</option>
-                <option value="2017">2017</option>
-                <option value="2018">2018</option>
                 <option value="2019">2019</option>
                 <option value="2020">2020</option>
                 <option value="2021">2021</option>
@@ -388,6 +385,11 @@
                 <option value="2023">2023</option>
                 <option value="2024">2024</option>
                 <option value="2025">2025</option>
+                <option value="2026">2026</option>
+                <option value="2027">2027</option>
+                <option value="2028">2028</option>
+                <option value="2029">2029</option>
+                <option value="2030">2030</option>
               </select>
             </div>
           </fieldset>
