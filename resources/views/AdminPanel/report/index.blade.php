d7ed0f5c9eab8787ad425c19263b1b51b1bf938d
roberto cordero <mago_ro_96@hotmail.com>
2020-08-06 12:55:43 -0700

    uno

--- resources/views/AdminPanel/report/index.blade.php
+++ resources/views/AdminPanel/report/index.blade.php
@@ -1,9 +1,47 @@
 @extends('AdminPanel.report.layout-report')
 
-@section('report-content')
+@section('report-content')  
 
-<div class="row">
-	hola
+<div class="card">
+  
+  <div class="card-body">
+
+    <div class="row">
+
+      <div class="col-md-12">
+
+        <h4>Reportes</h4> 
+
+      </div>
+
+      <div class="col-md-3 col-sm-12">
+
+        <div class="small-box bg-success">
+
+          <div class="inner">
+
+            <h3>Periodo</h3>
+
+            <p></p>
+
+          </div>
+
+          <div class="icon">
+
+            <i class="fa fa-user"></i>
+
+          </div>
+
+          <a href="" class="small-box-footer">Cambiar<i class="fas fa-arrow-circle-right"></i></a>
+
+        </div>
+        
+      </div>
+
+    </div>
+
+  </div>
+
 </div>
 
 @stop
