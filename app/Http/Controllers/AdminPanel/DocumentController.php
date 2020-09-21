027c798c0187b039b7be55843b1dc0339b8c89c4
Roberto Cordero <sonico24250@gmail.com>
2020-09-21 14:58:59 -0700

    prueba

--- app/Http/Controllers/AdminPanel/DocumentController.php
+++ app/Http/Controllers/AdminPanel/DocumentController.php
@@ -31,20 +31,24 @@ class DocumentController extends Controller
 
         foreach($alums_list as $key => $value)
         {
-            $alumn = User::find($value["id"]);
-            $countSuccess = Document::where([['alumn_id',$value["id"]],["status", 2]])->get()->count();
-            $querySicoes = selectSicoes("Alumno","AlumnoId",$alumn["id_alumno"]);           
+            try {
+                $alumn = User::find($value["id"]);
+                $countSuccess = Document::where([['alumn_id',$value["id"]],["status", 2]])->get()->count();
+                $querySicoes = selectSicoes("Alumno","AlumnoId",$alumn["id_alumno"]);           
 
-            $files = Document::select('document.*','document_type.name')->join('document_type','document.document_type_id','document_type.id')->where('alumn_id',$value["id"])->get();
+                $files = Document::select('document.*','document_type.name')->join('document_type','document.document_type_id','document_type.id')->where('alumn_id',$value["id"])->get();
 
-            array_push( $res["data"],[
-                "#"         => (count($alums_list)-($key+1)+1),
-                "Matricula" => $querySicoes[0]["Matricula"],
-                "Alumno"    => $alumn->name." ".$alumn->lastname,
-                "files"     => json_encode($files),
-                "countFiles"=> count($files),
-                "count"     => $countSuccess
-            ]);
+                array_push( $res["data"],[
+                    "#"         => (count($alums_list)-($key+1)+1),
+                    "Matricula" => $querySicoes[0]["Matricula"],
+                    "Alumno"    => $alumn->name." ".$alumn->lastname,
+                    "files"     => json_encode($files),
+                    "countFiles"=> count($files),
+                    "count"     => $countSuccess
+                ]);  
+            } catch(\Exception $e) {
+                dd($e);
+            }            
         }
         
         return response()->json($res);  
