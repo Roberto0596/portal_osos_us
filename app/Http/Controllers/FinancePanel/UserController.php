291727a222c5d058eb05ef0dce465058bc28ee70
Roberto Cordero <sonico24250@gmail.com>
2021-01-05 20:13:14 -0700

    klol

--- app/Http/Controllers/FinancePanel/UserController.php
+++ app/Http/Controllers/FinancePanel/UserController.php
@@ -16,4 +16,35 @@ class UserController extends Controller
         $current_user = AdminUser::find($current_id);
 		return view('FinancePanel.user.index')->with(["user"=>$current_user]);
     }
+
+    public function save(Request $request, AdminUser $user)
+    {
+        if ($request->input('password')!=null)
+        {
+            $user->password = bcrypt($request->input('password'));
+        }
+
+        if(isset($_FILES['newPicture']) && $_FILES["newPicture"]["name"]!="")
+        {
+            if ($user->photo == "img/computercenter/default/default.png")
+            {
+                $routePicture = ctrCrearImagen($_FILES["newPicture"],$user->id,"computercenter",100,120,false);
+                $user->photo = $routePicture;
+            }
+            else
+            {
+                unlink($user->photo);
+
+                $routePicture = ctrCrearImagen($_FILES["newPicture"],$user->id,"computercenter",100,120,true);
+                $user->photo = $routePicture;
+            }
+        }
+
+        $user->name = $request->input("name");
+        $user->email = $request->input("email");
+        $user->lastname = $request->input("lastname");
+        $user->save();
+        session()->flash("messages","success|Datos guardados correctamente");
+        return redirect()->route("computo.user");
+    }
 }
