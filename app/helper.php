0399cb69b7f577a3bace43e9c6ed1286cb0c007e
Roberto Cordero <sonico24250@gmail.com>
2021-02-03 23:33:10 -0700

    lol

--- app/helper.php
+++ app/helper.php
@@ -953,82 +953,84 @@ function current_group($id_alumno) {
 
     $sicoesAlumn = $alumn->getSicoesData();
 
-    $stateData = selectSicoes("Estado","EstadoId", $sicoesAlumn["EstadoDom"]);
-    $state = count($stateData) != 0 ? $stateData[0] : "No Disponible";
+    if ($sicoesAlumn) {
+      $stateData = selectSicoes("Estado","EstadoId", $sicoesAlumn["EstadoDom"]);
+      $state = count($stateData) != 0 ? $stateData[0] : "No Disponible";
 
-    $periodData = selectSicoes("Periodo","PeriodoId",$debit->period_id);
-    $period = count($periodData) != 0 ? $periodData[0] : "No Disponible";
+      $periodData = selectSicoes("Periodo","PeriodoId",$debit->period_id);
+      $period = count($periodData) != 0 ? $periodData[0] : "No Disponible";
 
-    $formatterES = new NumberFormatter("es", NumberFormatter::SPELLOUT);
+      $formatterES = new NumberFormatter("es", NumberFormatter::SPELLOUT);
 
-    $location = "No Disponible";
-    if(isset($sicoesAlumn["Localidad"]) && !empty($sicoesAlumn["Localidad"]) ){
-      $location= $sicoesAlumn["Localidad"];
-    }
+      $location = "No Disponible";
+      if(isset($sicoesAlumn["Localidad"]) && !empty($sicoesAlumn["Localidad"]) ){
+        $location= $sicoesAlumn["Localidad"];
+      }
 
-    $carrer = getCarrera($sicoesAlumn["Matricula"])["carrera"];
+      $carrer = getCarrera($sicoesAlumn["Matricula"])["carrera"];
 
-    $ticketInfo = [
-      "ticketNum"      => getTicketNumber(),
-      "date"           => $date,
-      "enrollment"     => $sicoesAlumn["Matricula"],
-      "name"           => strtolower($alumn->name." ".$alumn->lastname),
-      "rfc"            => substr($sicoesAlumn["Curp"],0,10),
-      "group"          => current_group($alumn->id_alumno)["Nombre"],
-      "semester"       => current_group($alumn->id_alumno)["Semestre"],
-      "career"         => strtolower(normalizeChars($carrer)),
-      "location"       => $location == "No Disponibe" ? $location : strtolower(normalizeChars($location)." ".normalizeChars($state["Nombre"])),
-      "order"          => $debit->id_order,
-      "period"         => $period["Clave"],
-      "concept"        => $debit->description,
-      "amount"         => number_format($debit->amount, 2),
-      "strAmount"      => $formatterES->format($debit->amount),
-      "payment_method" => $debit->payment_method,
-      "secureStr"      => "Pendiente"
-    ];
+      $ticketInfo = [
+        "ticketNum"      => getTicketNumber(),
+        "date"           => $date,
+        "enrollment"     => $sicoesAlumn["Matricula"],
+        "name"           => strtolower($alumn->name." ".$alumn->lastname),
+        "rfc"            => substr($sicoesAlumn["Curp"],0,10),
+        "group"          => current_group($alumn->id_alumno)["Nombre"],
+        "semester"       => current_group($alumn->id_alumno)["Semestre"],
+        "career"         => strtolower(normalizeChars($carrer)),
+        "location"       => $location == "No Disponibe" ? $location : strtolower(normalizeChars($location)." ".normalizeChars($state["Nombre"])),
+        "order"          => $debit->id_order,
+        "period"         => $period["Clave"],
+        "concept"        => $debit->description,
+        "amount"         => number_format($debit->amount, 2),
+        "strAmount"      => $formatterES->format($debit->amount),
+        "payment_method" => $debit->payment_method,
+        "secureStr"      => "Pendiente"
+      ];
 
-    $html = view('Alumn.ticket.template',['ticketInfo'=>$ticketInfo])->render(); 
+      $html = view('Alumn.ticket.template',['ticketInfo'=>$ticketInfo])->render(); 
 
-    $namefile = ucwords($debit->description).time().'.pdf';
-    $defaultConfig = (new \Mpdf\Config\ConfigVariables())->getDefaults();
-    $fontDirs = $defaultConfig['fontDir'];
-    $defaultFontConfig = (new \Mpdf\Config\FontVariables())->getDefaults();
-    $fontData = $defaultFontConfig['fontdata'];
-    $mpdf = new Mpdf([
-        'fontDir' => array_merge($fontDirs, [
-            public_path() . '/fonts',
-        ]),
-        'fontdata' => $fontData + [
-            'arial' => [
-                'R' => 'arial.ttf',
-                'B' => 'arialbd.ttf',
-            ],
-        ],
-        'default_font' => 'arial',
-        "format" => [210,297],
-    ]);
-   
-    $mpdf->SetDisplayMode('fullpage');
-    $mpdf->WriteHTML($html);
+      $namefile = ucwords($debit->description).time().'.pdf';
+      $defaultConfig = (new \Mpdf\Config\ConfigVariables())->getDefaults();
+      $fontDirs = $defaultConfig['fontDir'];
+      $defaultFontConfig = (new \Mpdf\Config\FontVariables())->getDefaults();
+      $fontData = $defaultFontConfig['fontdata'];
+      $mpdf = new Mpdf([
+          'fontDir' => array_merge($fontDirs, [
+              public_path() . '/fonts',
+          ]),
+          'fontdata' => $fontData + [
+              'arial' => [
+                  'R' => 'arial.ttf',
+                  'B' => 'arialbd.ttf',
+              ],
+          ],
+          'default_font' => 'arial',
+          "format" => [210,297],
+      ]);
+     
+      $mpdf->SetDisplayMode('fullpage');
+      $mpdf->WriteHTML($html);
 
-    $alumnData = $alumn->getSicoesData();
-    $path = $alumnData["Matricula"];
+      $alumnData = $alumn->getSicoesData();
+      $path = $alumnData["Matricula"];
 
-    Storage::disk('ticket_uploads')->makeDirectory($path);
+      Storage::disk('ticket_uploads')->makeDirectory($path);
 
-    try {     
-      $mpdf->Output("tickets/".$path."/".$namefile,"F");
-    } catch(\Exception $e) {
-      $mpdf->Output(public_path()."/". "tickets/".$path."/".$namefile,"F");
-    }
+      try {     
+        $mpdf->Output("tickets/".$path."/".$namefile,"F");
+      } catch(\Exception $e) {
+        $mpdf->Output(public_path()."/". "tickets/".$path."/".$namefile,"F");
+      }
 
-    $ticket = new Ticket();
-    $ticket->concept = ucwords($debit->description);
-    $ticket->alumn_id = $alumn->id;
-    $ticket->debit_id = $debit->id;
-    $ticket->route = "tickets/".$path."/".$namefile;
-    $ticket->created_at = $is_masive ? $debit->created_at : time();
-    $ticket->save();  
+      $ticket = new Ticket();
+      $ticket->concept = ucwords($debit->description);
+      $ticket->alumn_id = $alumn->id;
+      $ticket->debit_id = $debit->id;
+      $ticket->route = "tickets/".$path."/".$namefile;
+      $ticket->created_at = $is_masive ? $debit->created_at : time();
+      $ticket->save();
+    }    
 }
 
 function getAlumnByEnrollment($enrollment)
